30ac966ac0452adb5c25a6d4646d3edffe75a40c
Альберт Енгалычев <Albert.engalychev@gmail.com>
2022-10-18 17:16:45 +0300

    Initial commit

---
+++ src/main/java/com/example/learningrestfull/LearningrestfullApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningrestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningrestfullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningrestfullApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learningrestfull/LearningrestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningrestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningrestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
