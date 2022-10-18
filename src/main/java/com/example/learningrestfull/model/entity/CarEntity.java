88f2fe12440ae2302a04b1b1d727f8d83c26d2e0
Альберт Енгалычев <Albert.engalychev@gmail.com>
2022-10-18 21:23:05 +0300

    add entity

---
+++ src/main/java/com/example/learningrestfull/model/entity/CarEntity.java
@@ -0,0 +1,38 @@
+package com.example.learningrestfull.model.entity;
+
+import com.example.learningrestfull.model.StatusCar;
+import lombok.Getter;
+import lombok.Setter;
+
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name="car")
+@Getter
+@Setter
+public class CarEntity {
+
+    @Id
+    @Column(name = "uuid")
+    public UUID uuid;
+
+    @Column(name = "model")
+    public String model;
+
+    @Column(name="gov_number")
+    public String govNumber;
+
+    @Column(name = "mileage")
+    public int mileage;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    public StatusCar status;
+
+    @ManyToOne
+    @Column(name = "owner")
+    public CarEntity owner;
+}
+
---
+++ src/main/java/com/example/learningrestfull/model/entity/DriverEntity.java
@@ -0,0 +1,31 @@
+package com.example.learningrestfull.model.entity;
+
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import com.example.learningrestfull.model.DrivingLicenseCategory;
+import lombok.Getter;
+import lombok.Setter;
+
+@Table(name="driver")
+@Getter
+@Setter
+public class DriverEntity {
+    @Id
+    @Column(name = "uuid")
+    public UUID uuid;
+
+    @Column(name = "name")
+    public String name;
+
+    @Column(name = "age")
+    public int age;
+
+    @Column(name = "creating")
+    public OffsetDateTime creating;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="driver_lic")
+    public DrivingLicenseCategory driverLicenseCategory;
+}
