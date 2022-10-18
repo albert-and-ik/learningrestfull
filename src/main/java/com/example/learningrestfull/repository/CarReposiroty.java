19154f6bf07f83322eb9d7ffd2fc84af0682da11
Альберт Енгалычев <Albert.engalychev@gmail.com>
2022-10-18 21:53:02 +0300

    fixed a typo

--- src/main/java/com/example/learningrestfull/repository/CarReposiroty.java
+++ src/main/java/com/example/learningrestfull/repository/CarReposiroty.java
@@ -1,9 +1,9 @@
 package com.example.learningrestfull.repository;
 
-import com.example.learningrestfull.model.entity.DriverEntity;
+import com.example.learningrestfull.model.entity.CarEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface CarReposiroty extends JpaRepository<DriverEntity, UUID> {
+public interface CarReposiroty extends JpaRepository<CarEntity, UUID> {
 }
