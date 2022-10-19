b4bc4808fe5d7d48052aa9b9d30ffab06b15e602
Альберт Енгалычев <Albert.engalychev@gmail.com>
2022-10-19 19:37:23 +0300

    Added annotation @Service

--- src/main/java/com/example/learningrestfull/service/CarService.java
+++ src/main/java/com/example/learningrestfull/service/CarService.java
@@ -4,10 +4,12 @@ import com.example.learningrestfull.model.StatusCar;
 import com.example.learningrestfull.model.dto.CarDto;
 import com.example.learningrestfull.model.dto.CarShortDto;
 import com.example.learningrestfull.model.dto.NewCarDto;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public interface CarService {
 
     List<CarShortDto> getAll();
--- src/main/java/com/example/learningrestfull/service/DriverService.java
+++ src/main/java/com/example/learningrestfull/service/DriverService.java
@@ -3,10 +3,12 @@ package com.example.learningrestfull.service;
 import com.example.learningrestfull.model.dto.DriverDto;
 import com.example.learningrestfull.model.dto.DriverShortDto;
 import com.example.learningrestfull.model.dto.NewDriverDto;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public interface DriverService {
     List<DriverShortDto> getAll();
 
