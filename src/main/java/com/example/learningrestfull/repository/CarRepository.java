3229a1221fb3ee0714eadb25aeeccb9e4d183a2a
Альберт Енгалычев <Albert.engalychev@gmail.com>
2022-10-21 13:54:13 +0300

    inheritance from BaseRepository

--- src/main/java/com/example/learningrestfull/repository/CarRepository.java
+++ src/main/java/com/example/learningrestfull/repository/CarRepository.java
@@ -1,13 +1,9 @@
 package com.example.learningrestfull.repository;
 
 import com.example.learningrestfull.model.entity.CarEntity;
-import org.springframework.data.domain.Example;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-import java.util.UUID;
+@Repository
+public interface CarRepository extends BaseRepository<CarEntity> {
 
-public interface CarRepository extends JpaRepository<CarEntity, UUID> {
-    @Override
-    Optional<CarEntity> findById(UUID uuid);
 }
--- src/main/java/com/example/learningrestfull/repository/DriverRepository.java
+++ src/main/java/com/example/learningrestfull/repository/DriverRepository.java
@@ -1,12 +1,9 @@
 package com.example.learningrestfull.repository;
 
 import com.example.learningrestfull.model.entity.DriverEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-import java.util.UUID;
+@Repository
+public interface DriverRepository extends BaseRepository<DriverEntity> {
 
-public interface DriverRepository extends JpaRepository<DriverEntity, UUID> {
-    @Override
-    Optional<DriverEntity> findById(UUID uuid);
 }
